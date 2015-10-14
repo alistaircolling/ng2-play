89a73e9f15097897837e7d22d492bef7b9962885
alistaircolling <alistair.colling@gmail.com>
2015-10-14 11:54:56 +0100

    added browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
 
 var PATHS = {
     src: 'src/**/*.ts',
     typings: 'node_modules/angular2/bundles/typings/angular2/angular2.d.ts'
 };
 
-gulp.task('clean', function (done) {
+gulp.task('clean', function(done) {
     var del = require('del');
     del(['dist'], done);
 });
 
-gulp.task('ts2js', function () {
+gulp.task('ts2js', function() {
     var typescript = require('gulp-typescript');
     var tsResult = gulp.src([PATHS.src, PATHS.typings])
         .pipe(typescript({
@@ -24,19 +25,38 @@ gulp.task('ts2js', function () {
     return tsResult.js.pipe(gulp.dest('dist'));
 });
 
-gulp.task('play', ['ts2js'], function () {
-    var http = require('http');
-    var connect = require('connect');
-    var serveStatic = require('serve-static');
-    var open = require('open');
-
-    var port = 9000, app;
+gulp.task('serve', ['ts2js'], function() {
+    /* var http = require('http');*/
+    //var connect = require('connect');
+    //var serveStatic = require('serve-static');
+    /*var open = require('open');*/
 
     gulp.watch(PATHS.src, ['ts2js']);
 
-    app = connect().use(serveStatic(__dirname));
-    http.createServer(app).listen(port, function () {
-        open('http://localhost:' + port);
+    browserSync({
+        port: 3000,
+        files: ['index.html', 'dist/*.js'],
+        injectChanges: true,
+        logFileChanges: false,
+        logLevel: 'silent',
+        notify: true,
+        reloadDelay: 0,
+        server: {
+            baseDir: '',
+          /*  middleware: superstatic({*/
+                //debug: false
+            /*})*/
+        }
     });
-});
 
+
+    //var port = 9000,
+    //app;
+
+
+    //app = connect().use(serveStatic(__dirname));
+    //http.createServer(app).listen(port, function() {
+    //open('http://localhost:' + port);
+    /*});*/
+});
+gulp.task('default', ['serve']);
--- src/hello.ts
+++ src/hello.ts
@@ -3,7 +3,7 @@ import {Component, bootstrap} from 'angular2/angular2';
 @Component({
     selector: 'hello-app',
     template: `
-        <h1>Hello, {{name}}!</h1>
+        <h1>Hi there , {{name}}!</h1>
         Say hello to: <input [value]="name" (input)="name = $event.target.value">
     `
 })
